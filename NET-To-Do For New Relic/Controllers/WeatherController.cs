73c0f7be49b1ba830c8b37292c61e0c1384c4b12
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-10-09 09:19:39 -0700

    .Net To-Do for New Relic initial commit
    Copy existing dockerized .NET To-Do app and add two external API endpoints to allow users to access a third party API through the app.

---
+++ NET-To-Do For New Relic/Controllers/WeatherController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TodoApi.Controllers
+{
+    
+    public class WeatherResponse
+    {
+        public object[] consolidated_weather { get; set; }
+        // public string Summary { get; set; }
+        // public string Temperature { get; set; }
+    }
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        
+        
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Welcome to the Weather endpoint. Navigate to `/portland` or `/dublin` to see local weather!";
+        }
+        
+        [HttpGet("Portland")]
+        public async Task<IActionResult> GetPortlandWeather()
+        {
+            using (var client = new HttpClient())
+            {
+                try 
+                {
+                    client.BaseAddress = new Uri("https://www.metaweather.com/");
+                    var response = await client.GetAsync($"/api/location/2475687");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    // using dynamic type lets us parse through the data and send back only what we want
+                    dynamic rawWeather = JsonConvert.DeserializeObject<WeatherResponse>(stringResult);
+                    string summary = rawWeather.consolidated_weather[0].weather_state_name;
+                    double rawCelsius = rawWeather.consolidated_weather[0].the_temp;
+                    // temperature rounded up to the nearest 10ths place
+                    double temperature_celsius = Math.Ceiling(rawCelsius*10)/10;
+                    double rawFahrenheit = (rawCelsius * 9/5) + 32;
+                    double temperature_fahrenheit = Math.Ceiling(rawFahrenheit*10)/10;
+                    string date = rawWeather.consolidated_weather[0].applicable_date;
+                     return Ok(new {
+                        date,
+                        summary,
+                        temperature_celsius,
+                        temperature_fahrenheit
+                    });
+                }
+                catch (HttpRequestException HttpRequestException)
+                {
+                    return BadRequest($"Error getting your Portland weather summary: {HttpRequestException.Message}");
+                }
+            }
+        }
+
+        [HttpGet("Dublin")]
+        public async Task<IActionResult> GetDublinWeather()
+        {
+            using (var client = new HttpClient())
+            {
+                try 
+                {
+                    client.BaseAddress = new Uri("https://www.metaweather.com/");
+                    var response = await client.GetAsync($"/api/location/560743");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    // using dynamic type lets us parse through the data and send back only what we want
+                    dynamic rawWeather = JsonConvert.DeserializeObject<WeatherResponse>(stringResult);
+                    string summary = rawWeather.consolidated_weather[0].weather_state_name;
+                    double rawCelsius = rawWeather.consolidated_weather[0].the_temp;
+                    // temperature rounded up to the nearest 10ths place
+                    double temperature_celsius = Math.Ceiling(rawCelsius*10)/10;
+                    double rawFahrenheit = (rawCelsius * 9/5) + 32;
+                    double temperature_fahrenheit = Math.Ceiling(rawFahrenheit*10)/10;
+                    string date = rawWeather.consolidated_weather[0].applicable_date;
+                     return Ok(new {
+                        date,
+                        summary,
+                        temperature_celsius,
+                        temperature_fahrenheit
+                    });
+                }
+                catch (HttpRequestException HttpRequestException)
+                {
+                    return BadRequest($"Error getting your Dublin weather summary: {HttpRequestException.Message}");
+                }
+            }
+        }
+
+    }
+}
---
+++ NET-To-Do For New Relic/README.md
@@ -0,0 +1,19 @@
+# .NET To-Do List For New Relic
+
+This application is a .NET Core MVC web application with a back end API and a basic user interface. The database is stored temporarily in memory for the purposes of allowing the app to spin up in several different test environments.
+
+For purposes of testing distributed tracing and external calls, there are also two additional endpoints available that make an external API call before returning data:
+* `/api/weather/portland`
+*`/api/weather/dublin`
+
+Run locally by hitting `F5` in VS Code
+
+### Instructions for running the app with Dockerfile
+0. `dotnet publish --runtime debian-x64`
+1. `docker build . -t todoapi`
+2. `docker run -d -p 8080:80 --name todoapi todoapi`
+3. Go to `localhost:8080` to access the app in a web browser.
+
+### Other Commands:
+* To list the process ID: `docker ps`
+* To remove the container: `docker rm -f [container ID]`
