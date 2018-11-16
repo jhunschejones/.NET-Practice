3fa1db9a4ee96420f52f7ed9db4806f53b9e8534
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-11-16 12:51:13 -0800

    Update `/execute` controller

--- dotnet-core-app/Controllers/ExecuteController.cs
+++ dotnet-core-app/Controllers/ExecuteController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace TodoApi.Controllers
 {
@@ -15,7 +16,28 @@ namespace TodoApi.Controllers
         [HttpPost]
         public string Post([FromBody] string content)
         {
-            return content;
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage();
+            client.BaseAddress = new Uri("localhost::5005");
+
+            StringBuilder responseBuilder = new StringBuilder();
+
+            dynamic json = JsonConvert.DeserializeObject(content);
+
+            foreach (var route in json.action)
+            {
+                // get the http method
+                request.Method = json.action.method;
+                // get the route
+                request.RequestUri = json.action.route;
+                // TODO make a request and store the response
+                // using `SendAsync` becasue HttpRequestMesasage lets you set the method and uri
+                dynamic routeResponse = client.SendAsync(request);
+
+                // adding response 
+                responseBuilder.Append(routeResponse);
+            }
+            return responseBuilder.ToString();
         }
 
         [HttpGet]
